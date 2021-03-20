77370f6cb4f9922bbb2cb277a26dcebcedd4cbb9
hjjja <jahj30734@gmail.com>
2021-03-20 20:12:09 +0900

    mino location설정, 떨어지는 도형 초기출력,

--- tetris_2021/tetris_2021/PrintTemp.h
+++ tetris_2021/tetris_2021/PrintTemp.h
@@ -3,11 +3,12 @@
 #include <Windows.h>
 
 //shape, direc //level, score 구조체
-struct tempInfo { int shape; int direc; }; typedef tempInfo;
+struct minoInfo { int shape; int direc; }; typedef minoInfo;
 struct gradeInfo { int level; int score; }; typedef gradeInfo;
+struct locationInfo { int left_x; int right_x; int bottom_y; int top_y; } typedef locationInfo;
 
 enum Shape { BAR, CHAIR_L, CHAIR_R,SQUARE,Z_L,Z_R,T};
-enum Direc {UP,RIGHT,DOWN,LEFT};
+//enum Direc {UP,RIGHT,DOWN,LEFT};
 enum PrintIndex {BarHoriz, BarVertical, ZLVertical, ZLHoriz, ZRVertical, ZRHoriz, Square, ChairL0, ChairL1, ChairL2, ChairL3,
 																		ChairR0, ChairR1, ChairR2, ChairR3,  T0, T1, T2, T3 };
 
@@ -18,90 +19,108 @@ void gotoxy(int x, int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-
-void PrintTemp(tempInfo temp, int x, int y) {
-	int tempInfoIndex = temp.shape+temp.direc;
-	switch (tempInfoIndex) {
+//현재 좌표를 받아서 화면에 도형 출력, location 정보 set
+void PrintTemp(minoInfo mino, int x, int y, locationInfo* location) {
+	int minoInfoIndex = mino.shape+ mino.direc;
+	switch (minoInfoIndex) {
 	case BarHoriz:
-		gotoxy(x - 2, y); printf("□□□□"); break;
+		gotoxy(x - 2, y); printf("□□□□"); *location = {x-2,x+4,y,y}; break;
 	case BarVertical:
-		for (int i = -1; i < 3; i++) { gotoxy(x, y+i); printf("□"); }break;
-
+		for (int i = -1; i < 3; i++) { gotoxy(x, y+i); printf("□"); }
+		*location = { x,x,y+2,y-1 };
+		break;
 	case ZLVertical:
 		gotoxy(x+2, y-1); printf("□");
 		gotoxy(x, y); printf("□□");
 		gotoxy(x, y+1); printf("□");
+		*location = { x,x+2,y+1,y-1 };
 		break;
 	case ZLHoriz:
 		gotoxy(x - 2, y); printf("□□");
 		gotoxy(x, y + 1); printf("□□");
+		*location = { x - 2,x + 2,y+1,y };
 		break; 
 	case ZRVertical:
 		gotoxy(x, y-1); printf("□");
 		gotoxy(x, y); printf("□□");
 		gotoxy(x+2, y+1); printf("□");
+		*location = { x,x+2,y+1,y-1};
 		break;
 	case ZRHoriz:
 		gotoxy(x, y); printf("□□");
 		gotoxy(x-2, y+1); printf("□□");
+		*location = {x-2,x + 2,y+1,y};
 		break;
 	case Square:
 		gotoxy(x, y); printf("□□");
 		gotoxy(x, y+1); printf("□□");
+		*location = {x,x+2,y+1,y };
 		break;
 	case ChairL0:
 		gotoxy(x - 2, y); printf("□□□");
 		gotoxy(x - 2, y+1); printf("□");
+		*location = { x-2,x+2,y+1,y };
 		break;
 	case ChairL1:
 		gotoxy(x, y - 1); printf("□□");
 		gotoxy(x+2, y); printf("□");
 		gotoxy(x+2, y + 1); printf("□");
+		*location = { x,x+2,y+1,y-1 };
 		break;
 	case ChairL2:
 		gotoxy(x+2, y); printf("□");
 		gotoxy(x-2, y+1); printf("□□□");
+		*location = { x-2,x+2,y+1,y };
 		break;
 	case ChairL3:
 		gotoxy(x, y - 1); printf("□");
 		gotoxy(x, y); printf("□");
 		gotoxy(x, y + 1); printf("□□");
+		*location = { x,x+2,y+1,y-1 };
 		break;
 	case ChairR0:
 		gotoxy(x-2, y); printf("□□□");
 		gotoxy(x+2, y+1); printf("□");
+		*location = { x-2,x+2,y+1,y };
 		break;
 	case ChairR1:
 		gotoxy(x+2, y - 1); printf("□");
 		gotoxy(x+2, y); printf("□");
 		gotoxy(x, y + 1); printf("□□");
+		*location = { x,x+2,y+1,y-1 };
 		break;
 	case ChairR2:
 		gotoxy(x-2, y); printf("□");
 		gotoxy(x-2, y + 1); printf("□□□");
+		*location = { x-2,x+2,y+1,y };
 		break;
 	case ChairR3:
 		gotoxy(x, y - 1); printf("□□");
 		gotoxy(x, y); printf("□");
 		gotoxy(x, y + 1); printf("□");
+		*location = { x,x + 2,y + 1,y-1 };
 		break;
 	case T0:
 		gotoxy(x - 2, y); printf("□□□");
 		gotoxy(x, y + 1); printf("□");
+		*location = { x-2,x + 2,y + 1,y };
 		break;
 	case T1:
 		gotoxy(x, y - 1); printf("□");
 		gotoxy(x - 2, y); printf("□□");
 		gotoxy(x, y + 1); printf("□");
+		*location = { x - 2,x,y + 1,y-1 };
 		break;
 	case T2:
 		gotoxy(x, y-1); printf("□");
 		gotoxy(x - 2, y); printf("□□□");
+		*location = { x - 2,x + 2,y,y-1 };
 		break;
 	case T3:
 		gotoxy(x, y - 1); printf("□");
 		gotoxy(x, y); printf("□□");
 		gotoxy(x, y + 1); printf("□");
+		*location = { x,x + 2,y + 1,y-1 };
 		break;
 	}
 }
--- tetris_2021/tetris_2021/tetris_2021.cpp
+++ tetris_2021/tetris_2021/tetris_2021.cpp
@@ -10,11 +10,16 @@
 #define g_yMin 7
 #define g_yMax 26
 
+#define center_x 32
+#define center_y 7
+
 #define next_x 52
 #define next_y 11
 
 int ground[44][27] = { 0 };
+enum Key { k_left=75, k_right=77, k_rotate=72, k_down=80 };
 
+//초기화면, tetris 출력
 void PrintGameStart() {
 	//T
 	gotoxy(23, 10);	printf("□□□□");
@@ -54,27 +59,14 @@ void PrintGameStart() {
 	gotoxy(65, 14);	printf("□□□");
 	
 	gotoxy(42, 24);	printf("- GAME START");
+	gotoxy(70, 30);	printf("ver. 2021");
 	gotoxy(44, 26);	printf("> ENTER");
+
 	getchar(); system("cls");//뭐든 입력하면 화면 지움
 }
 
-//이미 내려놓은 도형 ■로 출력
-void ColoringTemp() {
-	for (int j = g_yMin; j <= g_yMax; j++)
-	{
-		for (int i = g_xMin; i <= g_xMax; i += 2)
-		{
-			if (ground[i][j] == 2)
-			{
-				gotoxy(i, j);
-				printf("■");
-			}
-		}
-	}
-}
-
 //게임 틀 출력 (+ next temp, level, score)
-void SetGameGround(tempInfo next, gradeInfo grade) {
+void SetGameGround(minoInfo next, gradeInfo grade, locationInfo* location) {
 	
 	char print_1[45] = "□□□□□□□□□□□□□□□□□□□□□□";
 	char print_2[26] = "□□\t\t\t\b\b\b\b□□\t\t\b\b\b\b□□";
@@ -95,40 +87,353 @@ void SetGameGround(tempInfo next, gradeInfo grade) {
 	else if (grade.score > 1000) gotoxy(51, 25);
 	else gotoxy(52, 25);
 	printf("%d", grade.score);
+
 	//다음도형 출력
-	//gotoxy(32, 8);
-	//print_next(shape_next, direc_next, 36, 8);
 	gotoxy(52, 8);
 	printf("NEXT");
+	//next 예쁜 위치에 출력하기 위한 if문
+	if ((next.shape + next.direc) % 2 == 1)
+		PrintTemp(next, next_x + 1, next_y, location);
+	else PrintTemp(next, next_x, next_y, location);
 	
-	
+}
+
+//이미 내려놓은 도형 ■로 출력
+void ColoringTemp() {
+	for (int j = g_yMin; j <= g_yMax; j++)
+	{
+		for (int i = g_xMin; i <= g_xMax; i += 2)
+		{
+			if (ground[i][j] == 2)
+			{
+				gotoxy(i, j);
+				printf("■");
+			}
+		}
+	}
+}
+
+//내려가는 도형 □로 출력
+void PrintDropTemp() {
+	for (int j = g_yMin; j <= g_yMax; j++)
+	{
+		for (int i = g_xMin; i <= g_xMax; i += 2)
+		{
+			if (ground[i][j] == 1)
+			{
+				gotoxy(i, j);
+				printf("□");
+			}
+		}
+	}
 }
 
 //떨굴 도형 모양, 방향 설정
-void setTempInfo(tempInfo *temp) {
-	srand(time(NULL));
-	temp->shape = rand() % 7;
-	if (temp->shape < 3) temp->direc = rand() % 2;
-	else if (temp->shape == 3) temp->direc = 0;
-	else temp->direc = rand() % 4;
+void SetMinoInfo(minoInfo *mino) {
+	mino->shape = rand() % 7;
+	if (mino->shape < 3) mino->direc = rand() % 2;
+	else if (mino->shape == 3) mino->direc = 0;
+	else mino->direc = rand() % 4;
 }
+
+//방향키 입력에 따라 동작
+void CheckKey(locationInfo* location) {
+	int input = 0;
+	//방향 키 입력 받아오기
+	if (_kbhit())
+	{
+		input = _getch();
+		if (input == 224)
+		{
+			do {
+				input = _getch();
+			} while (input == 224);
+		}
+
+		switch (input)
+		{
+		case k_left:
+			if (location->left_x >= 24)
+			{
+				//지우기, location 정보 새로고침
+				location->left_x -= 2;
+				location->right_x -= 2;
+
+				/*
+				uaaaa = shift_l(shape, *direc, *x, y, max_x, background);//지우는거 새로 만들어야할듯
+				if (uaaaa > 0) {
+					*x -= spacee;
+					system("cls");
+				}
+				*/
+			}
+			break;
+		case k_right:
+			if (location->right_x <= 42)//얘도
+			{
+				//지우기, location 정보 새로고침
+				location->left_x += 2;
+				location->right_x += 2;
+				/*
+				uaaaa = shift_r(shape, *direc, *x, y, max_x, background);
+				if (uaaaa > 0) {
+					*x += spacee;
+					system("cls");
+				}*/
+			}
+			break;
+		case k_down:
+			*input_down = 5;
+			break;
+		case k_rotate:
+			//	system("cls");
+			system("cls");
+			switch (shape)
+			{
+			case 0:
+				delete_bar(shape, *direc, *x, y, background, 0);
+
+				break;
+			case 1:
+				delete_a1(shape, *direc, *x, y, background, 0);
+
+				break;
+			case 2:
+				delete_a2(shape, *direc, *x, y, background, 0);
+
+				break;
+			case 3:
+				delete_square(shape, *direc, *x, y, background, 0);
+
+				break;
+			case 4:
+				delete_b1(*direc, *x, y, background, 0);
+
+				break;
+			case 5:
+				delete_b2(*direc, *x, y, background, 0);
+
+				break;
+			case 6:
+				delete_o(*direc, *x, y, background, 0);
+				break;
+			}
+			if (*direc == 3)
+				*direc = 0;
+			else
+				(*direc)++;//돌리면 멈춘다,,, 지우기 해야함
+			break;//돌았ㅇ을때 겹치는게 없으면 돌고 아님 못돌고
+		}
+	}
+}
+
+/*
+void check_input(tempInfo temp,locationInfo location,int shape, int* direc, int* x, int y, int* max_x, int background[28][24], int* input_down, int flag_1, int flag_2)
+{
+	int input = 0;
+	int uaaaa = 0;
+	if (_kbhit())
+	{
+		input = _getch();
+		if (input == 224)
+		{
+			do {
+				input = _getch();
+			} while (input == 224);
+		}
+		switch (input)
+		{
+		case k_left:
+			//system("cls");
+			if (location.left_x >= 24)
+			{
+				uaaaa = shift_l(shape, *direc, *x, y, max_x, background);//지우는거 새로 만들어야할듯
+				if (uaaaa > 0) {
+					*x -= spacee;
+					system("cls");
+				}
+			}
+			break;
+		case k_right:
+			//	system("cls");
+			if (location.right_x <= 42)//얘도
+			{
+				uaaaa = shift_r(shape, *direc, *x, y, max_x, background);
+				if (uaaaa > 0) {
+					*x += spacee;
+					system("cls");
+				}
+			}
+			break;
+		case k_down:
+			*input_down = 5;
+			break;
+		case k_rotate:
+			//	system("cls");
+			system("cls");
+			switch (shape)
+			{
+			case 0:
+				delete_bar(shape, *direc, *x, y, background, 0);
+
+				break;
+			case 1:
+				delete_a1(shape, *direc, *x, y, background, 0);
+
+				break;
+			case 2:
+				delete_a2(shape, *direc, *x, y, background, 0);
+
+				break;
+			case 3:
+				delete_square(shape, *direc, *x, y, background, 0);
+
+				break;
+			case 4:
+				delete_b1(*direc, *x, y, background, 0);
+
+				break;
+			case 5:
+				delete_b2(*direc, *x, y, background, 0);
+
+				break;
+			case 6:
+				delete_o(*direc, *x, y, background, 0);
+				break;
+			}
+			if (*direc == 3)
+				*direc = 0;
+			else
+				(*direc)++;//돌리면 멈춘다,,, 지우기 해야함
+			break;//돌았ㅇ을때 겹치는게 없으면 돌고 아님 못돌고
+		}
+
+	}
+}*/
+/*
+void SetCurrentLocation(int tempInfoIndex,locationInfo* location) {
+	switch (tempInfoIndex) {
+		//min_x = x-2
+	case BarHoriz:
+	case ZLHoriz:
+	case ZRHoriz:
+	case ChairL0:
+	case ChairL2:
+	case ChairR0:
+	case ChairR2:
+
+	//case BarHoriz:
+		//gotoxy(x - 2, y); printf("□□□□"); break;
+	case BarVertical:
+		for (int i = -1; i < 3; i++) { gotoxy(x, y + i); printf("□"); }break;
+
+	case ZLVertical:
+		gotoxy(x + 2, y - 1); printf("□");
+		gotoxy(x, y); printf("□□");
+		gotoxy(x, y + 1); printf("□");
+		break;
+	//case ZLHoriz:
+		//gotoxy(x - 2, y); printf("□□");
+		//gotoxy(x, y + 1); printf("□□");
+		//break;
+	case ZRVertical:
+		gotoxy(x, y - 1); printf("□");
+		gotoxy(x, y); printf("□□");
+		gotoxy(x + 2, y + 1); printf("□");
+		break;
+	//case ZRHoriz:
+		//gotoxy(x, y); printf("□□");
+		//gotoxy(x - 2, y + 1); printf("□□");
+		//break;
+	case Square:
+		gotoxy(x, y); printf("□□");
+		gotoxy(x, y + 1); printf("□□");
+		break;
+	//case ChairL0:
+		//gotoxy(x - 2, y); printf("□□□");
+		//gotoxy(x - 2, y + 1); printf("□");
+		//break;
+	case ChairL1:
+		gotoxy(x, y - 1); printf("□□");
+		gotoxy(x + 2, y); printf("□");
+		gotoxy(x + 2, y + 1); printf("□");
+		break;
+	//case ChairL2:
+	//	gotoxy(x + 2, y); printf("□");
+	//	gotoxy(x - 2, y + 1); printf("□□□");
+	//	break;
+	case ChairL3:
+		gotoxy(x, y - 1); printf("□");
+		gotoxy(x, y); printf("□");
+		gotoxy(x, y + 1); printf("□□");
+		break;
+	case ChairR0:
+		gotoxy(x - 2, y); printf("□□□");
+		gotoxy(x + 2, y + 1); printf("□");
+		break;
+	case ChairR1:
+		gotoxy(x + 2, y - 1); printf("□");
+		gotoxy(x + 2, y); printf("□");
+		gotoxy(x, y + 1); printf("□□");
+		break;
+	case ChairR2:
+		gotoxy(x - 2, y); printf("□");
+		gotoxy(x - 2, y + 1); printf("□□□");
+		break;
+	case ChairR3:
+		gotoxy(x, y - 1); printf("□□");
+		gotoxy(x, y); printf("□");
+		gotoxy(x, y + 1); printf("□");
+		break;
+	case T0:
+		gotoxy(x - 2, y); printf("□□□");
+		gotoxy(x, y + 1); printf("□");
+		break;
+	case T1:
+		gotoxy(x, y - 1); printf("□");
+		gotoxy(x - 2, y); printf("□□");
+		gotoxy(x, y + 1); printf("□");
+		break;
+	case T2:
+		gotoxy(x, y - 1); printf("□");
+		gotoxy(x - 2, y); printf("□□□");
+		break;
+	case T3:
+		gotoxy(x, y - 1); printf("□");
+		gotoxy(x, y); printf("□□");
+		gotoxy(x, y + 1); printf("□");
+		break;
+	}
+}
+*/
 
 int main()
 {
+	//게임창 세팅
 	system("mode con cols=100 lines=40 | title TETRIS"); // cols:가로 lines:세로
 	PrintGameStart();
 
-	tempInfo current = { 0,0 };
-	tempInfo next = { 0,0 };
+	//초기화
+	minoInfo current = { 0,0 };
+	minoInfo next = { 0,0 };
 	gradeInfo grade = { 0,0 };
-	setTempInfo(&next);
-	SetGameGround(next,grade);
+	locationInfo location = { 0,0,0,0 };
+	int currentCenterX = 42;
+	int currentCenterY = 7;
 
-	//next 예쁜 위치에 출력하기 위한 if문
-	if((next.shape+next.direc)%2==1)
-		PrintTemp(next, next_x+1, next_y);
-	else PrintTemp(next, next_x, next_y);
+	//게임 화면 세팅
+	//srand(time(NULL));
+	SetMinoInfo(&next);
+	SetGameGround(next,grade, &location);
+	SetMinoInfo(&current);
+	PrintTemp(current, center_x, center_y, &location);
 
+	gotoxy(30, 17);
+	printf("minx= %d maxx= %d miny= %d maxy= %d", location.left_x, location.right_x, location.bottom_y, location.top_y);
+	//while (1) {
+
+
+	//}
 	getchar();
 	
 }
